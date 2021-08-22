8f8acf5a9e70e80578f2bda5fa9d75dfe50a7575
John Lollar <jlollar45@gmail.com>
2021-08-22 18:10:25 -0500

    Added safari view implementation

---
+++ SwiftUI AppleFrameworks/SafariView.swift
@@ -0,0 +1,20 @@
+//
+//  SafariView.swift
+//  SwiftUI AppleFrameworks
+//
+//  Created by ParkingPal on 8/22/21.
+//
+
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    
+    let url: URL
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {}
+}
