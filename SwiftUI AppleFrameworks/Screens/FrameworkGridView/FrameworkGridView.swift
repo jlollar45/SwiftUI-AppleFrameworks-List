ef2a7f8bb61eb7cf5c65cf191ac4cda8091cb55a
John Lollar <jlollar45@gmail.com>
2021-08-22 18:41:58 -0500

    Changed GridView to List

---
+++ SwiftUI AppleFrameworks/Screens/FrameworkGridView/FrameworkGridView.swift
@@ -0,0 +1,34 @@
+//
+//  FrameworkGridView.swift
+//  SwiftUI AppleFrameworks
+//
+//  Created by ParkingPal on 8/22/21.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    
+    @StateObject var viewModel = FrameworkGridViewModel()
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(MockData.frameworks) { framework in
+                    NavigationLink(destination: FrameworkDetailView(framework: framework, isShowingDetailView: $viewModel.isShowingDetailView)) {
+                        FrameworkTitleView(framework: framework)
+                    }
+                }
+            }
+            .navigationTitle("Apple Frameworks")
+        }
+        .accentColor(Color(.label))
+    }
+}
+
+struct FrameworkGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+            .preferredColorScheme(.dark)
+    }
+}
