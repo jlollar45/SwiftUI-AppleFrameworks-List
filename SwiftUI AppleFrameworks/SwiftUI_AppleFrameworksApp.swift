c14bb75af0aab5d050b2614d9275453e2f927f29
John Lollar <jlollar45@gmail.com>
2021-08-22 15:54:12 -0500

    Initial Commit

---
+++ SwiftUI AppleFrameworks/SwiftUI_AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AppleFrameworksApp.swift
+//  SwiftUI AppleFrameworks
+//
+//  Created by ParkingPal on 8/22/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
