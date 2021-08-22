412d24e8ea73b5c87ed5dd8229f935e5435b9667
John Lollar <jlollar45@gmail.com>
2021-08-22 16:17:16 -0500

    Set up basic framework for Grid

---
+++ SwiftUI AppleFrameworks/FrameworkGridView.swift
@@ -0,0 +1,50 @@
+//
+//  FrameworkGridView.swift
+//  SwiftUI AppleFrameworks
+//
+//  Created by ParkingPal on 8/22/21.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+    
+    var body: some View {
+        LazyVGrid(columns: columns) {
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+        }
+    }
+}
+
+struct FrameworkGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+    }
+}
+
+struct FrameworkTitleView: View {
+    let name: String
+    let imageName: String
+    
+    var body: some View {
+        VStack {
+            Image("app-clip")
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text("App Clips")
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+    }
+}
